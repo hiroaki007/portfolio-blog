2baa5cce8be8fa9e16feee6591e7f07cbdb996e8
hiroaki <blackcoff0722@gmail.com>
2025-02-23 10:42:59 +0900

    フッターに日付データを追加しました。

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -1,7 +1,28 @@
+"use client"
+
+import { useState, useEffect } from "react";
+
+
 export default function Footer() {
+
+    const [date, setDate] = useState("");
+
+    useEffect(() => {
+        
+        const today = new Date();
+        const year = today.getFullYear();
+        const month = String(today.getMonth() + 1).padStart(2, "0"); // `getMonth()` は 0 (1月) から始まるため +1
+        const day = String(today.getDate()).padStart(2, "0");
+
+
+        setDate(`${year}年${month}月${day}日`);
+
+    }, []);
+
+
     return (
         <footer>
-            <p>&copy; My Portfolio. All rights reserved.</p>
+            <p>&copy; {date} My Portfolio. All rights reserved.</p>
         </footer>
     );
 }
